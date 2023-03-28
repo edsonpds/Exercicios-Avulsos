79bfd867e0081a53eaf1303596519239bee80176
Edson Peres <123039196+edsonpds@users.noreply.github.com>
2023-03-28 13:27:54 -0300

    Add files via upload

---
+++ MediaSalario/mediaSalario.java
@@ -0,0 +1,10 @@
+public class mediaSalario {
+    public static void main(String args[]) {
+   double n1 = 1080.00;
+   double n2 = 1500.90;
+   double n3 = 2800.00;
+   double n4 = 8500.90;
+   double media = (n1+n2+n3+n4)/4; 
+      System.out.println("A MEDIA SALARIAL É DE " + media + ".");
+    }
+}
